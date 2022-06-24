c7c08f2f9ba84ece81c5fcff43befae108243a40
Zack0ne <arezibzack0ne@gmail.com>
2022-06-24 16:02:51 +0800

    add animation

--- src/app/sensor/sensor.component.ts
+++ src/app/sensor/sensor.component.ts
@@ -9,37 +9,50 @@ import { HttpClient } from '@angular/common/http';
 export class SensorComponent implements OnInit {
   sensorStatus = false
   collectingData = true
+  loading = false
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit(): void {
+    this.loading = true
     this.http.get('').subscribe((payload)=>{
+      this.loading = false
       console.log('sucess')
       this.sensorStatus = true
     }, (error) => {
+      this.loading = false
       console.log('error')
       this.sensorStatus = true
     })
   }
 
   start(){
+    this.loading = true
     this.http.get('').subscribe((payload)=>{
+      this.loading = false
       console.log('sucess')
       this.sensorStatus = true
     }, (error) => {
       console.log('error')
+      this.loading = false
       this.sensorStatus = false
+      this.collectingData = true
     })
   }
   
   stop(){
+    this.loading = true
     this.http.get('').subscribe((payload)=>{
+      this.loading = false
       console.log('sucess')
       this.sensorStatus = true
+      
     }, (error) => {
+      this.loading = false
       console.log('error')
       this.sensorStatus = false
+      this.collectingData = false
     })
   }
   refresh(){
